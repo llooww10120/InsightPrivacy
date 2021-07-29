267919b7d052d47895ae92e7124345fb17d32eb4
Tan Timothy <tnsttyh@gmail.com>
2021-07-29 16:36:32 +0800

    data processing and convert to csv

---
+++ data_processing/data_processing.py
@@ -0,0 +1,42 @@
+import csv, os
+from tqdm import tqdm
+
+def readTable():
+    with open(os.path.join('data_processing', 'table.csv'), newline='',encoding="utf-8-sig") as f:
+
+
+        rows = csv.reader(f)
+        table = []
+
+        for row in rows:
+            for element in row:
+                table.append(element)
+        return table
+
+def compare(line, table):
+    s = []
+    output = []
+    for item in range(len(table)):
+        if table[item] in line:
+            s.append(line.find(table[item]))
+        else:
+            s.append(-1)
+    sorted_s = sorted(range(len(s)), key = lambda k : s[k]) 
+    for num in sorted_s:
+        if s[num] > 0:
+            output.append(table[num])
+    # print(output)
+    return output
+
+
+if __name__=="__main__":
+    table = readTable()
+
+    files = next(os.walk(os.path.join('cra', 'result')))[2]
+    for fl in tqdm(files, desc='conver to csv'):
+        # print(fl)
+        with open(os.path.join('data_processing', 'csv', '{}.csv'.format(fl[:3])), 'w', newline='', encoding="utf-8-sig") as f:
+            writer = csv.writer(f)
+            lines = open(os.path.join('cra', 'result', fl),'r', encoding='utf-8-sig').read().split('\n')
+            for line in lines:
+                writer.writerow([line, compare(line, table)])
