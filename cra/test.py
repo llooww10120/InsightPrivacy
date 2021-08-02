b81ae53e0d5a110b4479d6a25e5ebe8291b11cb5
Ming <llooww10120@gmail.com>
2021-08-02 18:11:16 +0800

    package

--- cra/test.py
+++
@@ -1,9 +0,0 @@
-import re
-li=[]
-with open("cra/result/021.txt","r",encoding='utf-8') as f:
-    a=f.readlines()
-for i in a:
-    if  "圖片" in i or "照片" in i:
-        li.append(a.index(i))
-
-print(li)
