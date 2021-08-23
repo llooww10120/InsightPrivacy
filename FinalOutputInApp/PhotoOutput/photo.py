26e3e764b1e96603167c32717230e1f22bb529bf
zerk895 <72378528+zerk895@users.noreply.github.com>
2021-08-23 17:56:44 +0800

    Update photo.py

--- FinalOutputInApp/PhotoOutput/photo.py
+++ FinalOutputInApp/PhotoOutput/photo.py
@@ -1,22 +1,74 @@
+#main.py
+
 # -*- coding: utf-8 -*-
-
-from PIL import ImageFont, ImageDraw, Image
-import cv2
 import numpy as np
+from cv2 import cv2
+from PIL import ImageFont, ImageDraw, Image
+
+ 
+def DrawPic(draw, font):
+
+    InsightPrivacy = "InsightPrivacy "
+    draw.text((350, 50), InsightPrivacy, font=font, align="center", fill=(41,36,33), size=1000)
+    
+    contact_data = '聯絡資訊: '
+    live_space = " 住址 " 
+    work_space = " 工作地址 "
+    mobile_number = " 行動電話\n號碼 "
+    phone_number = " 住家電話號碼 "
+    draw.multiline_text((10, 120), contact_data, fill=(41,36,33), font=font,align='left')
+    draw.multiline_text((110, 120), live_space, fill=(128,128,105), font=font)
+    draw.multiline_text((250, 120), work_space, fill=(128,128,105), font=font)
+    draw.multiline_text((410, 120), mobile_number, fill=(128,128,105), font=font,align='center')
+    draw.multiline_text((550, 120), phone_number, fill=(128,128,105), font=font) 
+
+    
+    financial_data = '財務資訊: '
+    financial_num = " 金融機構帳戶\n  之號碼與姓名 " 
+    credit_num = " 信用卡或簽帳卡\n之號碼 "
+    policy_num = " 保險單號碼 "
+    personal_num = " 個人之其他\n  號碼或帳戶 "
+    
+    draw.multiline_text((10, 220), financial_data, fill=(41,36,33), font=font,align='left')
+    draw.multiline_text((100, 220), financial_num, fill=(128,128,105), font=font,align='center')
+    draw.multiline_text((250, 220), credit_num, fill=(128,128,105), font=font,align='center')
+    draw.multiline_text((410, 220), policy_num, fill=(128,128,105), font=font)
+    draw.multiline_text((550, 220), personal_num, fill=(128,128,105), font=font,align='center')
+    
+    return
 
 
-# cv2讀取圖片
-img = cv2.imread('tmp.jfif')
-cv2img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)# cv2和PIL中颜色的hex码的储存顺序不同 
-pilimg = Image.fromarray(cv2img)
 
-# 透過PIL印出漢字
-draw = ImageDraw.Draw(pilimg) # 圖片上印字
-font = ImageFont.truetype("simhei.ttf", 20, encoding="utf-8") # 參數1:字體文件路徑, 參數2:字體大小
-        # 1.打印座標(x.y), 2.文本,    3.字體顏色,   4.字體
-draw.text((0, 0), "所使用的個人隱私", (41, 36, 33), font=font) 
 
-# PIL圖片轉 cv2 圖片
-cv2charimg = cv2.cvtColor(np.array(pilimg), cv2.COLOR_RGB2BGR)
-# cv2.imshow("图片", cv2charimg) # 汉字窗口标题显示乱码
-cv2.imshow("photo", cv2charimg)
+
+def CreatePic():
+
+    #創建背景
+    img = np.zeros((450, 450, 3), np.uint8) #黑背景
+    cv2img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) #如果是cv2讀取圖片才需
+    cv2img[:] = (255,255,240) # 上色
+    cv2img = cv2.resize(cv2img, (900, 500))
+    
+    # 將numpy陣列轉換為PIL影像
+    imgPil = Image.fromarray(cv2img)
+    
+    #在圖片上加入文字
+    draw = ImageDraw.Draw(imgPil)
+    font = ImageFont.truetype('simhei.ttf', 20, encoding='utf-8')
+    DrawPic(draw, font)
+
+    #將PIL轉回Numpy陣列
+    cv2charimg = cv2.cvtColor(np.array(imgPil), cv2.COLOR_RGB2BGR)
+   # cv2img = np.array(imgPil)
+
+    #呈現在螢幕上,除錯
+    cv2.imshow('Ouput', cv2charimg)
+    cv2.waitKey(100000)
+    cv2.destroyWindow('Ouput')
+
+    #輸出圖片檔
+    cv2.imwrite('result.jpg', cv2img)
+
+#避免引用檔案時
+if __name__== "__main__":
+    CreatePic()
