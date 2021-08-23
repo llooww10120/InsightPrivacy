f597404b101ef61506935b19a0272ec9d08d91e6
duncan881220 <duncan19991220@gmail.com>
2021-08-23 21:41:41 +0800

    UsageTest

---
+++ UsageTest/UsageTest.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import torch
+from transformers import BertTokenizer
+from IPython.display import clear_output
+from transformers import BertForSequenceClassification
+
+
+def usagetest(text,model):
+    model.eval()
+    predictions = None
+    tokens=tokenizer(text,padding=True,truncation=True,max_length=512)
+    ids=tokens["input_ids"]
+    segments=tokens["token_type_ids"]
+    masks =tokens["attention_mask"]
+    tokens_tensor=torch.tensor([ids])
+    segments_tensor=torch.tensor([segments])
+    masks_tensors=torch.tensor([masks])
+    outputs = model(input_ids=tokens_tensor, token_type_ids=segments_tensor, attention_mask=masks_tensors)
+    logits = outputs[0]
+    _, pred = torch.max(logits.data, 1)
+    return pred
+if __name__=="__main__":
+    PRETRAINED_MODEL_NAME = "bert-base-chinese"
+    tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)
+    NUM_LABELS = 2
+    model = BertForSequenceClassification.from_pretrained(PRETRAINED_MODEL_NAME, num_labels=NUM_LABELS)
+    clear_output()
+
+
+    text="您的資料將用於網站的維護及改善"
+    pred1 = usagetest(text,model)
+    text2 ="我們將不會隨意刪除您的資料"
+    pred2 = usagetest(text2,model)
+    print(f"""
+        text1:{int(pred1)}
+        text2:{int(pred2)}
+        """)
