2220571f3a809f8a998023beb94240455694a49f
Ming <llooww10120@gmail.com>
2021-08-25 15:33:31 +0800

    cra test

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, messages
 import configparser
+from cra import cra as ca
 app = Flask(__name__)
 
 config = configparser.ConfigParser()
@@ -12,6 +13,8 @@ config.read('config.ini')
 # LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+def getedata(url):
+    ca.cra(url)
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -31,7 +34,8 @@ def callback():
 # 
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-    message=ImageSendMessage(original_content_url="https://i.imgur.com/ae1ImwP.jpg",preview_image_url="https://i.imgur.com/ae1ImwP.jpg")
+    # message=ImageSendMessage(original_content_url="https://i.imgur.com/ae1ImwP.jpg",preview_image_url="https://i.imgur.com/ae1ImwP.jpg")
+    message=TextMessage(getedata)
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         line_bot_api.reply_message(event.reply_token,message)
 
--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-from FinalOutputInApp import uploadimgur as im
-import configparser 
-
-# print(im.upload2imgur("result.jpg","test"))
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-print(config.get('line-bot', 'channel_secret'))
+from cra import cra as ca
+a=ca.cra("https://1922.gov.tw/vas/info5.html")
+print(a)
